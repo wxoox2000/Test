5b77f48fd9db9897a183ca76c05bd757012468a9
wxoox2000 <wxoox2000@gmail.com>
2023-09-21 23:01:46 +0300

    third

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,8 @@ import App from "./App";
 import "./styles.css";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
